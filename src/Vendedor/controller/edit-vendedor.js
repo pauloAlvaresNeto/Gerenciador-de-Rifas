9db6f3a24e0f2154f2cd78d800418d413435d90a
pauloAlvaresNeto <paulo.alvares01@etec.sp.gov.br>
2022-08-02 22:05:14 -0300

    commit

---
+++ src/Vendedor/controller/edit-vendedor.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+    $('#table-vendedor').on('click', 'button.btn-edit', function(e) {
+
+        e.preventDefault()
+
+        // Alterar as informações do modal para apresentação dos dados
+
+        $('.modal-title').empty()
+        $('.modal-body').empty()
+
+        $('.modal-title').append('Visualização de registro')
+
+        let ID = `ID=${$(this).attr('id')}`
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            assync: true,
+            data: ID,
+            url: 'src/vendedor/model/view-vendedor.php',
+            success: function(dado) {
+                if (dado.tipo == "success") {
+                    $('.modal-body').load('src/vendedor/view/form-vendedor.html', function() {
+                        $('#NOME').val(dado.dados.NOME)
+                        $('#CELULAR').val(dado.dados.CELULAR)
+                        $('#LOGIN').val(dado.dados.CELULAR)
+                        $('#SENHA').val(dado.dados.CELULAR)
+                        $('#TIPO_ID').empty()
+                        $('#ID').val(dado.dados.ID)
+
+                        var TIPO_ID = dado.dados.TIPO_ID
+
+                        //Consultar todos os tipos cadastrados em banco de dados
+
+                        $.ajax({
+                            dataType: 'json',
+                            type: 'POST',
+                            assync: true,
+                            url: 'src/tipo/model/all-tipo.php',
+                            success: function(dados) {
+                                for (const result of dados) {
+                                    if (result.ID == TIPO_ID) {
+                                        $('#TIPO_ID').append(`<option value="${result.ID}" selected>${result.NOME}</option>`)
+                                    } else {
+                                        $('#TIPO_ID').append(`<option value="${result.ID}">${result.NOME}</option>`)
+                                    }
+                                }
+
+                            }
+                        })
+                    })
+                    $('.btn-save').removeAttr('data-operation', 'insert')
+                    $('.btn-save').show()
+                    $('#modal-vendedor').modal('show')
+                } else {
+                    Swal.fire({ // Inicialização do SweetAlert
+                        title: 'e-Rifa', // Título da janela SweetAler
+                        text: dado.mensagem, // Mensagem retornada do microserviço
+                        type: dado.tipo, // vendedor de retorno [success, info ou error]
+                        confirmButtonText: 'OK'
+                    })
+                }
+            }
+        })
+
+    })
+})
